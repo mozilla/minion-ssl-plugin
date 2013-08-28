b70a6949ccd0f63711fb361425691901c69f751c
zai <zai@z.ai>
2013-08-28 16:37:32 +0200

    bug fix

--- minion/plugins/ssl.py
+++ minion/plugins/ssl.py
@@ -83,7 +83,7 @@ class SSLPlugin(ExternalProcessPlugin):
         self.sslscan_stderr = ""
         u = urlparse.urlparse(self.configuration['target'])
         args = ["--no-failed"]
-        args = ["--xml=%s/report.xml" % self.work_directory]
+        args += ["--xml=%s/report.xml" % self.work_directory]
         args += ["%s:443" % u.hostname]
         self.spawn(sslscan_path, args)
 
